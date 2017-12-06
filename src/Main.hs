65938c7fd1a044265b65b4f0d66b2d3f0d298d76
Neil Parikh <parikh.neil@me.com>
2017-12-05 19:10:52 -0500

    support for adding new items (except on my day list)

--- src/Main.hs
+++ src/Main.hs
@@ -56,15 +56,19 @@ data Focus = Lists
 data CurrentState = CS {
     csLists :: NEZ.Zipper TodoList,
     csTasks :: Maybe (Z.Zipper Task),
-    csEditor :: Maybe (E.Editor String String)
+    csEditor :: Maybe (E.Editor String String),
+    nextID :: ID
 } deriving (Generic)
 
 csFocus :: CurrentState -> Focus
-csFocus (CS _ Nothing _) = Lists
-csFocus (CS _ (Just _) _) = Tasks
+csFocus (CS _ Nothing _ _) = Lists
+csFocus (CS _ (Just _) _ _) = Tasks
 
 setEditorTo :: Maybe (E.Editor String String) -> CurrentState -> CurrentState
 setEditorTo e = field @"csEditor" .~ e
+
+generateTasksZipper :: CurrentState -> CurrentState
+generateTasksZipper cs = cs & field @"csTasks" .~ (Just . Z.fromList . getCurrentTasks) (csLists cs)
 
 applyNTimes :: Int -> (a -> a) -> a -> a
 applyNTimes n f = foldr (.) id (replicate n f)
@@ -101,11 +105,17 @@ currentListName :: NEZ.Zipper TodoList -> String
 currentListName = listToName . NEZ.curr
 
 nameForCurrentItem :: CurrentState -> String
-nameForCurrentItem (CS _ (Just tasks) _) = maybe "" taskName (Z.getCurrent tasks)
-nameForCurrentItem (CS lists Nothing _) = currentListName lists
+nameForCurrentItem (CS _ (Just tasks) _ _) = if name == " " then "" else name -- special case for a new task
+    where
+    name = maybe "" taskName (Z.getCurrent tasks)
+nameForCurrentItem (CS (NEZ.Zipper _ (TL " " []) _) Nothing _ _) = "" -- special case for a new list
+nameForCurrentItem (CS lists Nothing _ _) = currentListName lists
+
+insertAtPosition :: Int -> a -> [a] -> [a]
+insertAtPosition i el xs = take i xs ++ [el] ++ drop i xs
 
 drawUI :: CurrentState -> [Widget String]
-drawUI cs@(CS z tasks editor) = [ui]
+drawUI cs@(CS z tasks editor _) = [ui]
     where
     focus = csFocus cs
     listsWidget = L.listMoveBy (NEZ.offset z) $ L.list "Lists" (Vec.fromList . listNames $ z) 1
@@ -132,7 +142,7 @@ drawUI cs@(CS z tasks editor) = [ui]
     ui = C.hCenter . C.vCenter $ hBox [box1 , if focus == Tasks then box2 else emptyBox]
 
 appEvent :: CurrentState -> T.BrickEvent String e -> T.EventM String (T.Next CurrentState)
-appEvent cs@(CS _ _ Nothing) (T.VtyEvent e) =
+appEvent cs@(CS _ _ Nothing _) (T.VtyEvent e) =
     case e of
         V.EvKey V.KUp []         -> M.continue . moveUp $ cs
         V.EvKey (V.KChar 'k') [] -> M.continue . moveUp $ cs
@@ -148,16 +158,15 @@ appEvent cs@(CS _ _ Nothing) (T.VtyEvent e) =
 
         V.EvKey (V.KChar 'd') [] -> M.continue . deleteCurrentItem $ cs
 
+        V.EvKey (V.KChar 'a') [] -> M.continue . addNewItem $ cs
+
         V.EvKey (V.KChar 'e') [] -> case csTasks cs of
             Just (Z.Zipper [] []) -> M.continue cs
-            _ -> let
-                focus = csFocus cs
-                editor = E.applyEdit TZ.gotoEOL $ E.editor ("edit " ++ show focus) (Just 1) (nameForCurrentItem cs)
-                in M.continue $ setEditorTo (Just editor) cs
+            _ -> M.continue . editCurrentItem $ cs
 
         V.EvKey V.KEsc [] -> M.halt cs
         _ -> M.continue cs
-appEvent cs@(CS _ _ (Just editor)) (T.VtyEvent e) =
+appEvent cs@(CS _ _ (Just editor) _) (T.VtyEvent e) =
     case e of
         V.EvKey V.KEsc [] -> M.continue $ setEditorTo Nothing cs
         V.EvKey V.KEnter [] -> M.continue $ setEditorTo Nothing $ updateCurrentItem cs (mconcat $ E.getEditContents editor)
@@ -166,35 +175,51 @@ appEvent cs@(CS _ _ (Just editor)) (T.VtyEvent e) =
             M.continue $ setEditorTo (Just editor') cs
 appEvent _ _ = error "FIXME: unhandled"
 
+editCurrentItem :: CurrentState -> CurrentState
+editCurrentItem cs = setEditorTo (Just editor) cs
+    where
+    focus = csFocus cs
+    editor = E.applyEdit TZ.gotoEOL $ E.editor ("edit " ++ show focus) (Just 1) (nameForCurrentItem cs)
+
+addNewItem :: CurrentState -> CurrentState
+addNewItem cs@(CS (NEZ.Zipper l curr r) Nothing _ _) = editCurrentItem $ cs & field @"csLists" .~ NEZ.Zipper (curr:l) (TL " " []) r
+addNewItem cs@(CS (NEZ.Zipper _ (TL name tasks) _) (Just tasksZipper) _ newID) = editCurrentItem . applyNTimes offset moveDown . generateTasksZipper $ cs & field @"csLists" %~ (NEZ.updateCurrent . const $ newList) & field @"nextID" %~ succ
+    where
+    newTask = Task newID " " False
+    newList = TL name (insertAtPosition offset newTask tasks)
+    offset = (Z.offset tasksZipper) + 1
+-- FIXME: figure out how to add items to the my day list
+addNewItem cs@(CS (NEZ.Zipper _ MyDay _) (Just _) _ _) = cs
+
 deleteCurrentItem :: CurrentState -> CurrentState
-deleteCurrentItem (CS _ (Just (Z.Zipper (_:_) [])) _) = error "invariant violation: zipper with non empty left but empty right"
-deleteCurrentItem (CS (NEZ.Zipper [] (TL _ _) []) Nothing _) = error "invariant violation: MyDay list does not exist"
-deleteCurrentItem cs@(CS (NEZ.Zipper _ MyDay _) Nothing _) = cs
-deleteCurrentItem cs@(CS (NEZ.Zipper l _ (x:xs)) Nothing _) = cs & field @"csLists" .~ NEZ.Zipper l x xs
-deleteCurrentItem cs@(CS (NEZ.Zipper (x:xs) _ []) Nothing _) = cs & field @"csLists" .~ NEZ.Zipper xs x []
-deleteCurrentItem cs@(CS _ (Just (Z.Zipper [] [])) _) = cs
-deleteCurrentItem (CS z (Just (Z.Zipper l (t:_))) edit) = CS newListsZipper (Just newTasksZipper) edit
+deleteCurrentItem (CS _ (Just (Z.Zipper (_:_) [])) _ _) = error "invariant violation: zipper with non empty left but empty right"
+deleteCurrentItem (CS (NEZ.Zipper [] (TL _ _) []) Nothing _ _) = error "invariant violation: MyDay list does not exist"
+deleteCurrentItem cs@(CS (NEZ.Zipper _ MyDay _) Nothing _ _) = cs
+deleteCurrentItem cs@(CS (NEZ.Zipper l _ (x:xs)) Nothing _ _) = cs & field @"csLists" .~ NEZ.Zipper l x xs
+deleteCurrentItem cs@(CS (NEZ.Zipper (x:xs) _ []) Nothing _ _) = cs & field @"csLists" .~ NEZ.Zipper xs x []
+deleteCurrentItem cs@(CS _ (Just (Z.Zipper [] [])) _ _) = cs
+deleteCurrentItem (CS z (Just (Z.Zipper l (t:_))) edit newID) = CS newListsZipper (Just newTasksZipper) edit newID
     where
     newListsZipper = deleteTask (taskID t) z
     newTasksZipper = applyNTimes (length l) Z.goRight . Z.fromList . getCurrentTasks $ newListsZipper
 
 updateCurrentItem :: CurrentState -> String -> CurrentState
-updateCurrentItem (CS _ (Just (Z.Zipper (_:_) [])) _) _ = error "invariant violation: zipper with non empty left but empty right"
-updateCurrentItem (CS _ (Just (Z.Zipper [] [])) _) _ = error "invariant violation: trying to update a non-existent task"
-updateCurrentItem cs@(CS (NEZ.Zipper _ MyDay _) Nothing _) _ = cs
-updateCurrentItem cs@(CS (NEZ.Zipper _ (TL _ tasks) _) Nothing _) newName = cs & field @"csLists" %~ (NEZ.updateCurrent . const $ TL newName tasks)
-updateCurrentItem (CS z (Just (Z.Zipper l (t:_))) edit) newName = CS newListsZipper (Just newTasksZipper) edit
+updateCurrentItem (CS _ (Just (Z.Zipper (_:_) [])) _ _) _ = error "invariant violation: zipper with non empty left but empty right"
+updateCurrentItem (CS _ (Just (Z.Zipper [] [])) _ _) _ = error "invariant violation: trying to update a non-existent task"
+updateCurrentItem cs@(CS (NEZ.Zipper _ MyDay _) Nothing _ _) _ = cs
+updateCurrentItem cs@(CS (NEZ.Zipper _ (TL _ tasks) _) Nothing _ _) newName = cs & field @"csLists" %~ (NEZ.updateCurrent . const $ TL newName tasks)
+updateCurrentItem (CS z (Just (Z.Zipper l (t:_))) edit newID) newName = CS newListsZipper (Just newTasksZipper) edit newID
     where
     newListsZipper = renameTask (taskID t) newName z
     newTasksZipper = applyNTimes (length l) Z.goRight . Z.fromList . getCurrentTasks $ newListsZipper
 
 moveUp :: CurrentState -> CurrentState
-moveUp cs@(CS _ (Just _) _) = cs & field @"csTasks" %~ fmap Z.goLeft
-moveUp cs@(CS _ Nothing _) = cs & field @"csLists" %~ NEZ.goLeft
+moveUp cs@(CS _ (Just _) _ _) = cs & field @"csTasks" %~ fmap Z.goLeft
+moveUp cs@(CS _ Nothing _ _) = cs & field @"csLists" %~ NEZ.goLeft
 
 moveDown :: CurrentState -> CurrentState
-moveDown cs@(CS _ (Just _) _) = cs & field @"csTasks" %~ fmap Z.goRight
-moveDown cs@(CS _ Nothing _) = cs & field @"csLists" %~ NEZ.goRight
+moveDown cs@(CS _ (Just _) _ _) = cs & field @"csTasks" %~ fmap Z.goRight
+moveDown cs@(CS _ Nothing _ _) = cs & field @"csLists" %~ NEZ.goRight
 
 moveLeft :: CurrentState -> CurrentState
 moveLeft cs = cs & field @"csTasks" .~ Nothing
@@ -239,7 +264,7 @@ listProjects :: TodoList
 listProjects = TL "Projects" [baz]
 
 initialState :: CurrentState
-initialState = CS (NEZ.Zipper [MyDay] listTodo [listProjects]) Nothing Nothing
+initialState = CS (NEZ.Zipper [MyDay] listTodo [listProjects]) Nothing Nothing (ID 5)
 
 main :: IO ()
 main = void $ M.defaultMain theApp initialState
